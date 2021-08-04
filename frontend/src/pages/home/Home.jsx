8b5b93ccc109314c0058d4b819f42cb8396461e2
José C. Martins <jcmartins81@outlook.com>
2021-08-04 19:22:21 -0300

    Construindo a Home

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import './home.css'
+import estoque from '../../assets/img/estoque.png'
 
 export default function Home() {
   return (
     <>
-      <div className="container">
-        <input type="text" name="email" id="email" />
+      <div className="home">
+        <aside>
+          <img src={estoque} alt="Imagem de uma prateleira cheia de produtos" />
+          <strong>Controle de Estoque - IESES</strong>
+        </aside>
+        <main>
+          <div className="main-content">
+            <h2>
+              Ja possui cadastro? <br />
+              Entre Aqui
+            </h2>
+            {/* eslint-disable-next-line react/button-has-type */}
+            <button className="acessar">Acessar</button>
+          </div>
+        </main>
       </div>
     </>
   )
