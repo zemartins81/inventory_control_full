44d4f2cec2cb4a3d5a883399bd0313a1c53c5441
José C. Martins <jcmartins81@outlook.com>
2021-08-13 17:13:42 -0300

    chore: add card list

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -1,13 +1,30 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import './card.css'
+import embreve from '../../assets/img/embreve.png'
 
 export default function Card({ product }) {
-  const { name } = product
-  console.log(product)
+  const { name, description, quantity, unit, unitPrice, amount } = product
+
   return (
     <div className="card">
-      <h1>{name}</h1>
+      <img src={embreve} alt="embreve" />
+      <strong>{name}</strong>
+      <p>Descrição: {description}</p>
+      <p>
+        Quantidade: {quantity} {unit}
+      </p>
+      <p>
+        Preço Unitário:{' '}
+        {unitPrice.toLocaleString('pt-br', {
+          style: 'currency',
+          currency: 'BRL',
+        })}
+      </p>
+      <p>
+        Saldo:{' '}
+        {amount.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+      </p>
     </div>
   )
 }
--- frontend/src/pages/products/Products.jsx
+++ frontend/src/pages/products/Products.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import { useEffect, useState } from 'react'
 import { getProductList } from '../../services/apiService'
 import './products.css'
@@ -5,12 +6,14 @@ import './products.css'
 import Header from '../../components/header/Header'
 import InsertProduct from '../../components/insertProduct/InsertProduct'
 import Card from '../../components/card/Card'
+import Button from '../../components/button/Button'
 
 export default function Products() {
   const [products, setProducts] = useState([])
   const [refreshList, setRefreshList] = useState(false)
   const [filterValue, setFilterValue] = useState('')
   const [insertProduct, setInsertProduct] = useState(false)
+  const [selectedProduct, setSelectedProduct] = useState({})
 
   useEffect(() => {
     const getAllProducts = async () => {
@@ -60,29 +63,34 @@ export default function Products() {
         value={filterValue}
         onChange={handleFilterChange}
       />
-      <aside>
-        <ul>
+      <div className="data">
+        <aside className="list-Products">
           {newListProducts.map((product) => (
             // eslint-disable-next-line no-underscore-dangle
-            <Card key={product._id} product={product} />
+
+            <Button
+              key={product._id}
+              onClick={() => setSelectedProduct(product)}
+            >
+              {product.name}
+            </Button>
           ))}
-        </ul>
-      </aside>
-      <main>
-        <ul>
-          {newListProducts.map((product) => (
-            // eslint-disable-next-line no-underscore-dangle
-            <Card key={product._id} product={product} />
-          ))}
-        </ul>
-      </main>
+        </aside>
+        <main className="product-details">
+          {selectedProduct.name ? (
+            <Card product={selectedProduct} />
+          ) : (
+            <h1>Selecione um produto para ver as mais detalhes</h1>
+          )}
+        </main>
+      </div>
     </>
   )
 
   return (
     <div className="container">
       <Header insertProduct={(value) => setInsertProduct(value)} />
-      <div className="data">{data}</div>
+      {data}
     </div>
   )
 }
